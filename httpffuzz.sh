8c2d597c8e30baf43ff742993a06f89c6aa08993
Thorfinn <komroncurabekov645@gmail.com>
2026-02-01 03:59:14 +0700

    WIP: handled some errors (#2) [in progress] handled input errors TODO: handle ouput errors

--- httpffuzz.sh
+++ httpffuzz.sh
@@ -1,9 +1,8 @@
-
 #!/bin/bash
 
 cleanup() {
 
-  echo -e "\033[1:33mUsage[!] CTRL-C detected, stopping scan ... \033[0m"
+  echo -e "\033[1;33mUsage[!] CTRL-C detected, stopping scan ... \033[0m"
   kill 0
   exit 1
 
@@ -14,12 +13,20 @@ trap cleanup INT
 url=""
 wordls=""
 
+if [[ $# -ne 4 ]]; then
+    echo -e "\033[1;31m[!] Incorrect number of arguments\033[0m"
+    echo -e "\033[1;33mUsage: $0 -w [wordlist] -u [URL]\033[0m"
+    exit 1
+fi
+
+
 if [[ "$1" == "-u" ]] && [[ "$3" == "-w"  ]]; then
-	url=$2
-	wordls=$4
-elif [[ "$1" == "-w" ]] && [[ "$3" == "-u" ]]; then
-	url=$4
-	wordls=$2
+	url="$2"
+	wordls="$4"
+elif [[ "$1" == "-w" ]] && [[ "$3" == "-u" ]]; 
+then
+	url="$4"
+	wordls="$2"
 
 else
 	echo -e "\033[1;33m Incorrect usage of script:\n httpffuz -w [wordlist] -u [Url]\033[0m"
@@ -27,3 +34,4 @@ else
 fi
 
 echo -e "Url: $url\nWordlist: $wordls"
+
